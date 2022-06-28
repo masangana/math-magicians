4480a9b29a2a62b6e72dd5a6d6f9e44ed70d65c4
masangana <alexmasangana@gmail.com>
2022-06-29 01:53:50 +0300

    add action on button

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import './calculator.css';
 import { btns, BTN_ACTIONS } from './btnConfig';
 
 const Calculator = () => {
+
+    const btnsRef = useRef(null)
+    const expRef = useRef(null)
+
+    useEffect(()=>{
+        const btns = Array.from(btnsRef.current.querySelectorAll('button'));
+
+        btns.forEach(e => e.style.height = e.offsetwidth + 'px')
+    },[])
+
+    const btnClick = (item) => {
+        console.log(item)
+    }
+
     return (
       <div className='calculator'>
         <div className="calculator__result">
-            <div className="calculator__result__exp"></div>
+            <div ref={expRef} className="calculator__result__exp">
+                999999
+            </div>
             <div className="calculator__result__exp"></div>
         </div>
-        <div className="calculator__btns">
+        <div ref={btnsRef} className="calculator__btns">
             {
                 btns.map((item, index)=>(
-                    <button key={index} className={item.class}>
+                    <button 
+                        key={index} 
+                        className={item.class}
+                        onClick={() => btnClick(item)}
+                    >
                         {item.display}
                     </button>
                 ))
