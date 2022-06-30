c2dcf70f1fbbf2a25711dee59c89d07fc9de65e1
masangana <alexmasangana@gmail.com>
2022-06-30 19:09:02 +0300

    refont with hooks

---
+++ src/components/btns.js
@@ -0,0 +1,121 @@
+
+import { v4 as uuidv4 } from 'uuid';
+
+const buttons = [
+    {
+      symbol: 'ACD',
+      id: uuidv4(),
+      name: 'AC',
+      color: 'Gray',
+    },
+    {
+      symbol: '+/-',
+      id: uuidv4(),
+      name: 'Inverter',
+      color: 'Gray',
+    },
+    {
+      symbol: '%',
+      id: uuidv4(),
+      name: 'Modulo',
+      color: 'Gray',
+    },
+    {
+      symbol: '÷',
+      id: uuidv4(),
+      name: 'Divide',
+      color: 'Orange',
+    },
+    {
+      symbol: '7',
+      id: uuidv4(),
+      name: 'Seven',
+      color: 'Gray',
+    },
+    {
+      symbol: '8',
+      id: uuidv4(),
+      name: 'Eight',
+      color: 'Gray',
+    },
+    {
+      symbol: '9',
+      id: uuidv4(),
+      name: 'Nine',
+      color: 'Gray',
+    },
+    {
+      symbol: 'x',
+      id: uuidv4(),
+      name: 'Multiply',
+      color: 'Orange',
+    },
+    {
+      symbol: '4',
+      id: uuidv4(),
+      name: 'Four',
+      color: 'Gray',
+    },
+    {
+      symbol: '5',
+      id: uuidv4(),
+      name: 'Five',
+      color: 'Gray',
+    },
+    {
+      symbol: '6',
+      id: uuidv4(),
+      name: 'Six',
+      color: 'Gray',
+    },
+    {
+      symbol: '-',
+      id: uuidv4(),
+      name: 'Minus',
+      color: 'Orange',
+    },
+    {
+      symbol: '1',
+      id: uuidv4(),
+      name: 'One',
+      color: 'Gray',
+    },
+    {
+      symbol: '2',
+      id: uuidv4(),
+      name: 'Two',
+      color: 'Gray',
+    },
+    {
+      symbol: '3',
+      id: uuidv4(),
+      name: 'Three',
+      color: 'Gray',
+    },
+    {
+      symbol: '+',
+      id: uuidv4(),
+      name: 'Plus',
+      color: 'Orange',
+    },
+    {
+      symbol: '0',
+      id: uuidv4(),
+      name: 'Zero',
+      color: 'Gray',
+    },
+    {
+      symbol: '.',
+      id: uuidv4(),
+      name: 'Dot',
+      color: 'Gray',
+    },
+    {
+      symbol: '=',
+      id: uuidv4(),
+      name: 'Equal',
+      color: 'Orange',
+    },
+  ];
+
+    export default buttons;
