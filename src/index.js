e97fe154069fffae96637cb592a9bb57af0a171a
Alex Masangana <alexmasangana@gmail.com>
2022-07-07 18:13:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import ReactDOM from 'react-dom/client.js';
+import ReactDOM from 'react-dom/client';
 // eslint-disable-next-line no-unused-vars
 import App from './App.js';
 
